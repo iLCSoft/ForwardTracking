98dccb3cf016d4857d5fe1b69daeab92ca4dc73c
tmadlener <thomas.madlener@desy.de>
2023-06-12 16:39:52 +0200

    Fix several warnings

--- include/ForwardTracking/EndcapHitSimple.h
+++ include/ForwardTracking/EndcapHitSimple.h
@@ -18,20 +18,19 @@ namespace KiTrackMarlin{
    public:
       
       EndcapHitSimple( float x , float y , float z , int layer , int phi, int theta, const SectorSystemEndcap* const sectorSystemEndcap );
-      
-      
+      EndcapHitSimple(const EndcapHitSimple&) = default;
+      EndcapHitSimple& operator=(const EndcapHitSimple&) = default;
+      ~EndcapHitSimple() = default;
       
       virtual const ISectorSystem* getSectorSystem() const { return _sectorSystemEndcap; };
-      
-      virtual ~EndcapHitSimple(){}
-      
+
    private:
       
-      int _layer;
-      int _phi;
-      int _theta;
+      int _layer{};
+      int _phi{};
+      int _theta{};
       
-      const SectorSystemEndcap* _sectorSystemEndcap;
+      const SectorSystemEndcap* _sectorSystemEndcap{nullptr};
       
       //void calculateSector(){ _sector = _sectorSystemEndcap->getSector( _side, _layer , _module , _sensor ); }
       
